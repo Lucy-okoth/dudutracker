fc628bc5a224136aa8675fa134704218434ec926
nkiro <nkiromuriuki10@gmail.com>
2024-03-25 10:31:22 +0300

    modified yarn.lock

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -13,18 +13,26 @@ const Navbar = () => {
     <AppBar variant="elevation" position="fixed" color="primary">
       <Container>
         <Toolbar disableGutters>
-          <Typography>
-            <Link
-              href="/"
-              style={{
-                textDecoration: "none",
-                color: "white",
-              }}
-              color="white"
-            >
-              {"<DuduTracker Logo>"}
-            </Link>
-          </Typography>
+          <Link href="/" passHref legacyBehavior>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <svg width="32" height="32">
+                <image
+                  xlinkHref="/images/logo.png"
+                  style={{
+                    borderRadius: "5px",
+                    height: "40px",
+                    width: "35px",
+                    paddingLeft: "1 rem",
+                    margin: "0 10px",
+                    display: "inline",
+                  }}
+                ></image>
+              </svg>
+              <Typography variant="h6" sx={{ paddingLeft: "0.5rem" }}>
+                DuduTracker
+              </Typography>
+            </Box>
+          </Link>
           <Box flexGrow={1}></Box>
           <Box>
             <Button sx={{ color: "#fff" }} href="/auth/login">
